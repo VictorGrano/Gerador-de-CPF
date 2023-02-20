bb10a9212c7c8abebf9e88b20b5d7740a6bd9326
VictorGrano <116359890+VictorGrano@users.noreply.github.com>
2023-02-20 07:20:41 -0300

    Atualização
    Código menor com o uso de funções

--- gerador_cpf.py
+++ gerador_cpf.py
@@ -1,34 +1,40 @@
 import random
 import os
 
+def pontua_cpf(cpf):
+    cpf_pontuado = cpf[:3] + '.' + cpf[3:6] + '.' + cpf[6:9] + '-' + cpf[9:]
+    return cpf_pontuado
+def gera_cpf():
+    cpf = ''
+    for i in range(9):
+        cpf += str(random.randint(0, 9))
+    contador1 = 10
+    resultado_digito1 = 0
+    for num in cpf:
+        resultado_digito1 += int(num) * contador1
+        contador1 -= 1
+    digito1 = (resultado_digito1 * 10) % 11
+    digito1 = digito1 if digito1 <= 9 else 0
+    cpf = cpf + str(digito1)
+    contador2 = 11
+    resultado_digito2 = 0
+    for num in cpf:
+        resultado_digito2 += int(num) * contador2
+        contador2 -= 1
+    digito2 = (resultado_digito2 * 10) % 11
+    digito2 = digito2 if digito2 <= 9 else 0
+    cpf = cpf + str(digito2)
+    cpf_final = pontua_cpf(cpf)
+    print(cpf_final)
+    return cpf_final
 print('Bem vindo ao gerador de cpf!')
 while True:
-    print('Digite uma opção')
-    print('1 - Gerar CPF com pontuação / 2 - Gerar CPF em massa / 3 - Sair')
+    print('Digite uma opção: \n 1 - Gerar CPF com pontuação / 2 - Gerar CPF em massa / 3 - Sair')
     opc = input('')
+    
     if opc == '1':
         os.system('cls')
-        cpf = ''
-        for i in range(9):
-            cpf += str(random.randint(0, 9))
-        contador1 = 10
-        resultado_digito1 = 0
-        for num in cpf:
-            resultado_digito1 += int(num) * contador1
-            contador1 -= 1
-        digito1 = (resultado_digito1 * 10) % 11
-        digito1 = digito1 if digito1 <= 9 else 0
-        cpf = cpf + str(digito1)
-        contador2 = 11
-        resultado_digito2 = 0
-        for num in cpf:
-            resultado_digito2 += int(num) * contador2
-            contador2 -= 1
-        digito2 = (resultado_digito2 * 10) % 11
-        digito2 = digito2 if digito2 <= 9 else 0
-        cpf = cpf + str(digito2)
-        cpf_gerado = cpf[:3] + '.' + cpf[3:6] + '.' + cpf[6:9] + '-' + cpf[9:]
-        print(cpf_gerado)
+        gera_cpf()
         continue
     if opc == '2':
         verifica = False
@@ -42,27 +48,7 @@ while True:
                 continue
         os.system('cls')
         for _ in range(qtd):
-            cpf = ''
-            for i in range(9):
-                cpf += str(random.randint(0, 9))
-            contador1 = 10
-            resultado_digito1 = 0
-            for num in cpf:
-                resultado_digito1 += int(num) * contador1
-                contador1 -= 1
-            digito1 = (resultado_digito1 * 10) % 11
-            digito1 = digito1 if digito1 <= 9 else 0
-            cpf = cpf + str(digito1)
-            contador2 = 11
-            resultado_digito2 = 0
-            for num in cpf:
-                resultado_digito2 += int(num) * contador2
-                contador2 -= 1
-            digito2 = (resultado_digito2 * 10) % 11
-            digito2 = digito2 if digito2 <= 9 else 0
-            cpf = cpf + str(digito2)
-            cpf_gerado = cpf[:3] + '.' + cpf[3:6] + '.' + cpf[6:9] + '-' + cpf[9:]
-            print(cpf_gerado)
+           gera_cpf()
         continue
     if opc == '3':
         break
